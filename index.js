df21a65fad00f71b750f5a6924b3de030f926056
mynul <homeexercise8@gmail.com>
2022-05-07 22:27:14 +0600

    unneccesary code delete

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-const res = require("express/lib/response");
+
+// middle ware
+
 app.use(cors())
 app.use(express.json())
 
